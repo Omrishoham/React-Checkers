11d8bf0180a6752c38442b8ee9bfefb477495302
Omrishoham <omri.s13@gmail.com>
2021-06-22 23:33:56 +0300

    add some functions for handling moves

--- src/App.js
+++ src/App.js
@@ -62,6 +62,96 @@ class App extends Component {
     });
   }
 
+  
+  //create an array of checkers(objects) on the board and return it
+  makeCheckers() {
+    let size = 8;//board size
+    let checkers = [];
+    let num = (size / 2) * 3;
+    let row = size - 1, col = 0;
+    for (let i = 0; i < num; i++) {
+      if (i && i % (size / 2) === 0) {
+        row--;
+        col = i == size ? 0 : 1;
+      }
+      checkers.push({ player: PLAYER_ONE, isKing: false, row: row, col: col, removed: false });
+      col += 2;
+    }
+    row = 0;
+    col = 1;
+    for (let i = 0; i < num; i++) {
+      if (i && i % (size / 2) === 0) {
+        row++;
+        col = i == size ? 1 : 0;
+      }
+      checkers.push({ player: PLAYER_TWO, isKing: false, row: row, col: col, removed: false });
+      col += 2;
+    }
+    return checkers;
+  }
+
+  selectSquare(row, col) {//activated when user select square in board
+    console.log("square row "+row+" column "+col);
+    let selected = this.state.choosedSquare;
+    if (this.canSelectChecker(row, col)) {//check if user select square with checker belong to himself or not
+      this.setSquare(row, col);//set choosed square in state
+    } else if (selected != null) {//check if user select square to move after he select the square he want to move from
+      this.handleMove(row, col);
+    }
+  }
+
+  handleMove(row, col) {
+    let board = this.state.board;
+    let checkers = this.state.checkers;
+    let selected = this.state.choosedSquare;
+    let startCheckerSelected = board[selected.row][selected.column];
+    if (!canMoveChecker(board,checkers,startCheckerSelected, row, col)) {//if move isnt in move legal so return and dont make move
+      console.log("illegal move");
+      return;
+    }
+
+    let isJump = this.isJumpMove(startCheckerSelected, row, col);
+    let becameKing = false;
+    this.moveChecker(startCheckerSelected, row, col);
+    if (!this.isKing(startCheckerSelected) && (this.getPlayer(startCheckerSelected) === PLAYER_ONE && row === 0) || (this.getPlayer(startCheckerSelected) === PLAYER_TWO && row === ((this.state.board.length) - 1))) {
+      console.log("making King....");
+      becameKing = true;
+      this.makeKing(startCheckerSelected);
+    }
+      this.setState({ board: board, currentPlayer: this.oppositePlayer(), choosedSquare: null });
+  }
+
+  canSelectChecker(row, column) {
+    let checkerID = this.state.board[row][column];
+    if (checkerID>=0 && checkerID<=23 && checkerID!=null) {//check if selected square is with checker
+     let checkerPlayer = this.state.checkers[checkerID].player;
+      return checkerPlayer === this.state.currentPlayer;//check if checker selecetd belong to the current player playing
+    }
+    return false;
+  }
+
+  setSquare(row, column) {
+    this.setState({ choosedSquare: { row: row, column: column } });
+  }
+
+  oppositePlayer() {
+    return (this.state.currentPlayer === PLAYER_ONE ? PLAYER_TWO : PLAYER_ONE)
+  }
+
+  //get all single moves and jump moves of checkers
+
+  hasMoves(player) {
+    let checkers = this.state.checkers;
+    let board = this.state.board;
+    let moves = getAllMoves(player,checkers,board);
+    return moves.jumps.length + moves.singles.length > 0;
+  }
+  
+  isJumpMove(checker, row) {
+    return Math.abs(this.state.checkers[checker].row - row) === 2;
+  }
+
+
 
   
 
